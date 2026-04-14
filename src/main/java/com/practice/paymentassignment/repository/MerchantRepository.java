591b98155bd5e7f72fe65dbdb8925975611c5b3f
KimSeungYong-hub <sm.19275@smartfintech.co.kr>
2026-04-14 16:57:05 +0900

    feat: Merchant 레포지토리 추가

---
+++ src/main/java/com/practice/paymentassignment/repository/MerchantRepository.java
@@ -0,0 +1,9 @@
+package com.practice.paymentassignment.repository;
+
+import com.practice.paymentassignment.entity.Merchant;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MerchantRepository extends JpaRepository<Merchant, Long> {
+
+    boolean existsByMerchantName(String merchantName);
+}
