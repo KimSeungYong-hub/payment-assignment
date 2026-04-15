1dd17d41f92281858e34c9b8b8aca3336f664b69
KimSeungYong-hub <sm.19275@smartfintech.co.kr>
2026-04-15 17:24:54 +0900

    feat: Payment orderId, status 상태 추가

--- src/main/java/com/practice/paymentassignment/entity/Payment.java
+++ src/main/java/com/practice/paymentassignment/entity/Payment.java
@@ -1,23 +1,56 @@
 package com.practice.paymentassignment.entity;
 
 import jakarta.persistence.*;
+import jdk.jshell.Snippet;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.OffsetDateTime;
 
+@Getter
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Payment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "merchant_id")
-    private Merchant merchantName;
+    private Merchant merchant;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
+
+    private int amount;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus status;
+
+    @Column(unique=true)
+    private String orderId;
+
     private OffsetDateTime dateTime;
 
+    @Builder
+    public Payment( Merchant merchant, int amount, User user, PaymentStatus status, String orderId) {
+        this.merchant = merchant;
+        this.amount = amount;
+        this.user = user;
+        this.status = status;
+        this.orderId = orderId;
+    }
+
+
+    public void complete(){
+        this.status = PaymentStatus.SUCCESS;
+    }
+
+    public void cancel(){
+        this.status = PaymentStatus.CANCELED;
+    }
 
 }
