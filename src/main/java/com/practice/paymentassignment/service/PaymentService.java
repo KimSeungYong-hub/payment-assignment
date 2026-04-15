f04d7cca0423b33379252bcb7e9ef12e4111bdba
KimSeungYong-hub <sm.19275@smartfintech.co.kr>
2026-04-15 17:27:45 +0900

    feat: PaymentService.java 결제 확인, 결제 요청 메서드 분리

--- src/main/java/com/practice/paymentassignment/service/PaymentService.java
+++ src/main/java/com/practice/paymentassignment/service/PaymentService.java
@@ -1,8 +1,12 @@
 package com.practice.paymentassignment.service;
 
+import com.practice.paymentassignment.dto.PaymentPrepareRequest;
+import com.practice.paymentassignment.dto.PaymentPrepareResponse;
 import com.practice.paymentassignment.dto.PaymentRequest;
+import com.practice.paymentassignment.dto.PaymentApproveResponse;
 import com.practice.paymentassignment.entity.Merchant;
 import com.practice.paymentassignment.entity.Payment;
+import com.practice.paymentassignment.entity.PaymentStatus;
 import com.practice.paymentassignment.entity.User;
 import com.practice.paymentassignment.exception.MerchantNotFoundException;
 import com.practice.paymentassignment.exception.UserNotFoundException;
@@ -13,6 +17,8 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @RequiredArgsConstructor
 @Service
 public class PaymentService {
@@ -22,18 +28,25 @@ public class PaymentService {
 
 
     @Transactional
-    public boolean requestPayment(PaymentRequest request){
+    public PaymentApproveResponse requestPayment(PaymentRequest request){
         String merchantName = request.getMerchantName();
+        String orderId = request.getOrderId();
+        Long merchantId = request.getMerchantId();
         Long userId = request.getUserId();
         int amount = request.getAmount();
-        validatePaymentStatus(orderId);
 
+        validatePaymentStatus(orderId);
         validateMerchant(merchantName);
-        User user = userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException(userId,"사용자를 찾을 수 없습니다."));
+
+        User user = userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException("사용자를 찾을 수 없습니다."));
         user.pay(amount);
 
+        Payment payment = paymentRepository.findByOrderId(orderId);
+        payment.complete();
 
-        return true;
+
+
+        return PaymentApproveResponse.from(true, "결제가 완료되었습니다.");
     }
 
     public void validateMerchant(String merchantName){
@@ -42,12 +55,40 @@ public class PaymentService {
         }
     }
 
-    public void validatePaymentStatus(Long orderId){
-        final Payment payment = paymentRepository.findById(orderId);
-        if(payment.status){
-            throw new MerchantNotFoundException("유효하지 않은 가맹점입니다.");
+    public void validatePaymentStatus(String orderId){
+        if(!paymentRepository.existsByOrderId(orderId)){
+            throw new MerchantNotFoundException("이미 처리 중이거나 완료된 주문입니다.");
         }
     }
 
 
+    public PaymentPrepareResponse preparePayment(PaymentPrepareRequest request) {
+        Long userId = request.getUserId();
+        Long merchantId = request.getMerchantId();
+        int amount = request.getAmount();
+        String orderId = generateOrderId(merchantId);
+
+        User user = userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException("사용자를 찾을 수 없습니다."));
+        Merchant merchant = merchantRepository.findById(merchantId).orElseThrow(() -> new MerchantNotFoundException("가맹점을 찾을 수 없습니다."));
+
+        String merchantName = merchant.getMerchantName();
+
+        Payment payment = Payment.builder()
+                .user(user)
+                .merchant(merchant)
+                .status(PaymentStatus.READY)
+                .amount(amount)
+                .orderId(orderId)
+                .build();
+
+        paymentRepository.save(payment);
+
+        return PaymentPrepareResponse.from(merchantName, amount, orderId);
+
+    }
+
+    private String generateOrderId(Long merchantId) {
+        String orderId = merchantId +"_ORDER_" + UUID.randomUUID();
+        return orderId;
+    }
 }
