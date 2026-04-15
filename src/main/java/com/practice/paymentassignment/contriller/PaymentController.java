4d128d17fb87829c3a38d33cda5d30f6dd73edb4
KimSeungYong-hub <sm.19275@smartfintech.co.kr>
2026-04-15 17:25:20 +0900

    feat: PaymentController 추가

---
+++ src/main/java/com/practice/paymentassignment/contriller/PaymentController.java
@@ -0,0 +1,29 @@
+package com.practice.paymentassignment.contriller;
+
+import com.practice.paymentassignment.dto.PaymentPrepareRequest;
+import com.practice.paymentassignment.dto.PaymentPrepareResponse;
+import com.practice.paymentassignment.dto.PaymentRequest;
+import com.practice.paymentassignment.dto.PaymentApproveResponse;
+import com.practice.paymentassignment.service.PaymentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/sft")
+@RequiredArgsConstructor
+@RestController
+public class PaymentController {
+    private final PaymentService paymentService;
+
+    @PostMapping("/prepare")
+    public ResponseEntity<PaymentPrepareResponse> preparePayment(@RequestBody PaymentPrepareRequest request){
+        PaymentPrepareResponse response = paymentService.preparePayment(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/approve")
+    public ResponseEntity<PaymentApproveResponse> approvePayment(@RequestBody PaymentRequest request){
+        PaymentApproveResponse response = paymentService.requestPayment(request);
+        return ResponseEntity.ok(response);
+    }
+}
