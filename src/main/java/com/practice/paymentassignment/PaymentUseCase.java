9b417a18097bd703598fefeb12749447e3f9c875
KimSeungYong-hub <sm.19275@smartfintech.co.kr>
2026-04-30 16:53:07 +0900

    refactoring: PaymentUseCase 결제 메서드 수정

--- src/main/java/com/practice/paymentassignment/PaymentUseCase.java
+++ src/main/java/com/practice/paymentassignment/PaymentUseCase.java
@@ -1,16 +1,15 @@
 package com.practice.paymentassignment;
 
-import com.practice.paymentassignment.domain.merchant.MerchatService;
+import com.practice.paymentassignment.domain.merchant.Merchant;
+import com.practice.paymentassignment.domain.merchant.service.MerchatService;
+import com.practice.paymentassignment.domain.payment.PaymentRequestEntity;
+import com.practice.paymentassignment.domain.user.User;
 import com.practice.paymentassignment.domain.user.UserService;
 import com.practice.paymentassignment.domain.wallet.WalletService;
 import com.practice.paymentassignment.dto.PaymentDto;
-import com.practice.paymentassignment.entity.Payment;
-import com.practice.paymentassignment.entity.PaymentRequestEntity;
-import com.practice.paymentassignment.entity.User;
-import com.practice.paymentassignment.entity.Wallet;
 import com.practice.paymentassignment.exception.InsufficientBalanceException;
 import com.practice.paymentassignment.exception.PaymentExpiredException;
-import com.practice.paymentassignment.service.PaymentService;
+import com.practice.paymentassignment.domain.payment.service.PaymentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -28,23 +27,26 @@ public class PaymentUseCase {
 
     @Transactional
     public PaymentDto.Prepare.Response readyPayment(PaymentDto.Prepare.Request request, String idempotencyKey) {
-
-
+        Merchant merchant = merchatService.findMerchant(request.getMerchantId());
+        return paymentService.savePaymentRequest(merchant, idempotencyKey);
     }
 
-
-
+    @Transactional
     public PaymentDto.Approve.Response confirmPayment(PaymentDto.Approve.Request request, Long userId) {
         PaymentRequestEntity paymentRequest = paymentService.findPaymentRequestWithLock(request.getPaymentId());
         BigDecimal totalAmount = paymentRequest.getTotalAmount();
+        User user = userService.findUser(userId);
+
         try{
             paymentRequest.verifyCanBeApproved(request.getPaymentId(), request.getAmount());
-            walletService.deduct(userId, request.getAmount());
+            walletService.deduct(userId, totalAmount);
+            paymentService.recordSuccess(paymentRequest, user, totalAmount);
+            return PaymentDto.Approve.Response.of(true, "결제가 완료되었습니다.");
         }catch (InsufficientBalanceException|PaymentExpiredException e){
-            User user = userService.findUser(userId);
-            paymentService.recordFailure(paymentRequest, user, totalAmount, e);
+            //Propagation.REQUIRES_NEW 메서드 데드락 위험성?? + db 커넥션 2개 소모됨
+            paymentService.recordFailure(paymentRequest, user, totalAmount, e.getMessage());
+            throw e;
         }
-        return PaymentDto.Approve.Response.of(true, "결제가 완료되었습니다.");
     }
 
 }
--- src/main/java/com/practice/paymentassignment/controller/PaymentController.java
+++ src/main/java/com/practice/paymentassignment/controller/PaymentController.java
@@ -2,9 +2,8 @@ package com.practice.paymentassignment.controller;
 
 import com.practice.paymentassignment.PaymentUseCase;
 import com.practice.paymentassignment.dto.PaymentDto;
-import com.practice.paymentassignment.service.PaymentService;
+import com.practice.paymentassignment.domain.payment.service.PaymentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,11 +12,12 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class PaymentController {
     private final PaymentUseCase paymentUseCase;
+    private final PaymentService paymentService;
 
     @PostMapping("/ready")
     public ResponseEntity<PaymentDto.Prepare.Response> readyPayment(@RequestBody PaymentDto.Prepare.Request request,
             @RequestHeader(value = "Idempotency-Key") String idempotencyKey) {
-        PaymentDto.Prepare.Response response = paymentService.readyPayment(request, idempotencyKey);
+        PaymentDto.Prepare.Response response = paymentUseCase.readyPayment(request, idempotencyKey);
         return ResponseEntity.ok(response);
     }
 
@@ -25,11 +25,6 @@ public class PaymentController {
     public ResponseEntity<PaymentDto.Approve.Response> confirmPayment(@RequestBody PaymentDto.Approve.Request request) {
         Long userId = 1L;
         PaymentDto.Approve.Response response = paymentUseCase.confirmPayment(request, userId);
-
-        if (!response.isSuccess()) {
-            // 비즈니스 실패 (잔액 부족 등) -> HTTP 400 상태 코드로 반환
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
-        }
         return ResponseEntity.ok(response);
     }
 
